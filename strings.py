5923a2c385f333d57644e0d0d0658de50089e75a
munenebrian <njerubrianmunene@gmail.com>
2023-01-06 21:12:36 +0300

    string in python

---
+++ strings.py
@@ -0,0 +1,19 @@
+# with strings we have multi line strings and single line strings.
+varA = "This is a car" #single string
+print(varA)
+varB = "This is a long sentence" \
+    "and we trying to break it" \
+        "because we know how" #multi string
+print(varB)
+# key to note and remember, a string is like an array in that we can acces every element of the string by their indexing
+name = 'Brian'
+print(name)
+print(name[0])
+# built in functions in python
+# we have the print() function it outputs anything put in it.
+# we have the input() function.
+print("Where do you live?")
+location = input()
+print("So you live in " + location + "!")
+# we also have len() which gives us the lenght value of the variable
+# str() this converts to a string int() converts to a integer float() converts to a float value
