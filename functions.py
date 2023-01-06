3d16fa1682a5c8198496710150521d2d40b583c4
munenebrian <njerubrianmunene@gmail.com>
2023-01-07 00:21:19 +0300

    functions in python

---
+++ functions.py
@@ -0,0 +1,12 @@
+# functions in pythons are defined with a key word def followed by an identifier (a name for example) then the body contains the code to be run when the function is called.
+
+def say_hello():
+    return "Hello there!"
+
+# with parameters
+def say_hello(you):
+    return "Hello" + you
+
+# type casting is the converting of oe data type into another. we have two types of coversions, implicits and explicits
+# implicits are automatic and happen within the python compiler.
+# explicit are done by the programmer using set functions provided in python.
