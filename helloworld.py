01407a12dbd8394004e90e38baf64e1bf18ecaed
munenebrian <njerubrianmunene@gmail.com>
2023-01-06 17:34:45 +0300

    learning python

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+print("hello world!")
+print("my name is Brian and i will be an engineer soon enough!")
+a = ('A', 45, 'Jimmy', 1)
+print(type(a))
+b = [1, 2, 3]
+print(type(b))
+c = 45
+print(type(c))
