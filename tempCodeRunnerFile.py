5fa5da5e2a578f47e1577341f5bdad10054896f2
munenebrian <njerubrianmunene@gmail.com>
2023-01-07 10:54:03 +0300

    typecasting

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+user_num_1 = input('First number is: ')
+# user_num_2 = input('Second number is: ')
+# user_sum = int(user_num_1) + int(user_num_2)
+# print(user_sum)
--- type_casting.py
+++ type_casting.py
@@ -8,5 +8,15 @@ num_1 = input('First number is: ')
 num_2 = input('Second number is: ')
 user_sum = float(num_1) + float(num_2)
 # print("The sum of: " + num_1 + " and " + num_2 + " is " + user_sum) this line here will throw an error
-# why? implicityl python can convertcompatible data types, ie. float and int but not a float and string.
+# why? implicitly python can convert compatible data types, ie. float and int but not a float and string.
 print("The sum of: " + str(num_1) + " and " + str(num_2) + " is " + str(user_sum))
+
+name = input('What is your name? ')
+str(name)
+print(f"Type of name variable is: " + str(name) + " It should be <class 'str'>")
+print(type(name))
+
+age = int(input('What is your age? '))
+print("Type of age variable is: " + str(age) + " It should be <class 'int'>")
+print(type(age))
+ # this was the challenge i did
