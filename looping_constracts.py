14d8085435e5df8cefd387d786b4d2d3c204570b
munenebrian <njerubrianmunene@gmail.com>
2023-01-09 17:29:10 +0300

    looping in python

--- looping_constracts.py
+++ looping_constracts.py
@@ -1,4 +1,15 @@
 str = 'looping'
 
-for item in str:
-     print(item)
+for item in str: # for is the kwy word of the for loop code. item is the variable in action
+     print(item) # and in is the specifier of where we want to loop over.
+
+favorites = ['banana', 'orange', 'strawberry', 'pineapple', 'avocado']
+
+for item in favorites:
+    print("I like this fruit...", item)
+
+
+for i in range(10):
+    print('looping..', i)
+
+# these are few examples of for looping in python.
