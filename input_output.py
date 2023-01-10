268416fb6f8627cecfaac6bdafb546db7e40b2f7
munenebrian <njerubrianmunene@gmail.com>
2023-01-10 22:35:34 +0300

    revisiting

--- input_output.py
+++ input_output.py
@@ -2,9 +2,10 @@ name = input("What is your name? ")
 num = input("PLease enter your age: ")
 place = input("Where do you live? ")
 print("So your name is "  + name + ", you are " + num + " years old, and live in " + place + ".")
-# here we have practised to use two keu functions in python, the input function and the output function.
+# here we have practised to use two key functions in python, the input function and the output function.
 # we need to mindful that when we use an input function, all our values are actually strings and if we need to do arithmetic calculation of integers we would need to convert them first.
 # we can also use string replacement for such examples
 str1 = input("What is your first name?")
 srt2 = input("What is your second name?")
 print("Hello {} {}".format(str1, srt2))
+ # for our inputs we used input() and print()
