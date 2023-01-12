08ccbeac4157bb7ad83061d2ceccd8ecbc8d7c09
munenebrian <njerubrianmunene@gmail.com>
2023-01-12 14:40:24 +0300

    more

--- nested_loops.py
+++ nested_loops.py
@@ -10,3 +10,6 @@ for x in list1 :
         count += 1
 
 print(count)
+# nested loops happen when a loop loops over another loop till the former is executed for the latter to end.
+# this happens when the first loop is outlined and the second depends on the first one. If the first one is executed, the second one is executed.
+# each condition in the first loop has to be met for the second loop to be finished.
