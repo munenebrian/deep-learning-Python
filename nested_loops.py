011967a4ed9ad531c91b356de6fb913536a16132
munenebrian <njerubrianmunene@gmail.com>
2023-01-16 09:13:05 +0300

    nested loops

--- nested_loops.py
+++ nested_loops.py
@@ -12,4 +12,16 @@ for x in list1 :
 print(count)
 # nested loops happen when a loop loops over another loop till the former is executed for the latter to end.
 # this happens when the first loop is outlined and the second depends on the first one. If the first one is executed, the second one is executed.
-# each condition in the first loop has to be met for the second loop to be finished.
+# each condition in the first loop has to be met for the second loop to be finished
+# .
+import time
+start_time = time.time()
+
+ # outer loop
+for i in range(100):
+    #inner loop
+    for j in range(100):
+        print(0, end = " ")
+    print()
+
+print(round((time.time() - start_time), 2))
